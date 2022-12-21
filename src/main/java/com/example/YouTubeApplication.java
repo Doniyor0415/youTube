a18ebe93373cfc5b52c1625c5dce99af1ca906c9
Doniyor Abdukarimov <108351501+Doniyor0415@users.noreply.github.com>
2022-12-21 15:06:42 +0300

    Update YouTubeApplication.java

--- src/main/java/com/example/YouTubeApplication.java
+++ src/main/java/com/example/YouTubeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YouTubeApplication {
 
+	//salom
 	public static void main(String[] args) {
 		SpringApplication.run(YouTubeApplication.class, args);
 	}
